361aaeb1f9d9d00c8cb31fe8fd45c310808aac07
AnnaMarandola <annamarandola@gmail.com>
2020-06-29 21:15:12 +0200

    get recipes by health filters

--- src/App.js
+++ src/App.js
@@ -1,32 +1,84 @@
 import React, { useEffect, useState } from 'react';
 import Recipe from './Recipe';
-import './App.css';
+// import './App.css';
+import Button from '@material-ui/core/Button';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+import { makeStyles } from '@material-ui/core/styles';
+import { TextField } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    margin: '4rem',
+    '& > *': {
+      margin: theme.spacing(1),
+    },
+    '& .MuiOutlinedInput-root': {
+      '& fieldset': {
+        borderColor: '#2a3eb1',
+      },
+      '&:hover fieldset': {
+        borderColor: '#757de8',
+      },
+      '&.Mui-focused fieldset': {
+        borderColor: '#002984',
+      },
+  },
+},  
+  instructions: {
+    fontWeight: 300,
+    color: '#2a3eb1',
+  },
+  searchForm: {
+    display: 'flex',
+    flexDirection: 'column',
+    marginTop: '2rem',
+  },
+  searchButton: {
+    marginTop: '2rem',
+    width: '80%',
+    marginLeft: '10%',
+  },
+  recipes: {
+    display: 'flex',
+    flexDirection: 'row',
+  }
+}));
+
+
 
 
 const App = () => {
+  const classes = useStyles()
 
-  const APP_ID = "a6a78483";
-  const APP_KEY = "985e5bd05d53928bc9882c6af27cac8f";
+const APP_ID = "a6a78483";
+const APP_KEY = "985e5bd05d53928bc9882c6af27cac8f";
 
 const [recipes, setRecipes] = useState([]);
 const [search, setSearch] = useState('');
 const [query, setQuery] = useState('');
+const [healthFilter, setHealthFilter] = useState('');
 
-  useEffect(() => {
-getRecipes();
-  }, [query]);
+useEffect(() => {
+  getRecipes();
+    }, [query]);
+    
+    const handleHealthFilters = event => {
+     setHealthFilter(event.currentTarget.value)
+    }
 
   const getRecipes = async () => {
     const response = await fetch (
-      `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
+      `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&health=${healthFilter}`
       );
     const data = await response.json();
     setRecipes(data.hits);
-
   };
 
   const updateSearch = event => {
-    setSearch(event.target.value)
+    setSearch(event.currentTarget.value)
   }
 
   const getSearch = event => {
@@ -35,25 +87,58 @@ getRecipes();
   }
 
 
+  console.log('recipes', recipes)
+  console.log('healthFilter', healthFilter)
+
   return (
     <div className="App">
-      {/* <header className="header"> */}
-        {/* <h1 className="title-app">Foodies scramble</h1> */}
-      <form onSubmit={getSearch} className="search-form">
-        <input className="search-bar" type="text" placeholder="enter an ingredient" value={search} onChange={updateSearch} />
-        <button className="search-button" type="submit">
+      { !healthFilter.length && 
+      <div className={classes.root}>
+      <h3 className={classes.instructions}>Renseignez votre allergie / intolerance:</h3>
+      <div className={classes.allergensSection}>
+      <ButtonGroup
+      orientation="vertical"
+      color="primary"
+      aria-label="vertical contained primary button group"
+      variant="text"
+      >
+      <Button onClick={ handleHealthFilters} value="peanut-free">Peanuts</Button>
+      <Button onClick={ handleHealthFilters} value="crustacean-free">Crustaceans</Button>
+      <Button onClick={ handleHealthFilters} value="dairy-free">Lactose</Button>
+      <Button onClick={ handleHealthFilters} value="egg-free">Eggs</Button>
+      <Button onClick={ handleHealthFilters} value="fish-free">Fish</Button>
+      <Button onClick={ handleHealthFilters} value="soy-free">Soy</Button>
+      <Button onClick={ handleHealthFilters} value="gluten-free">Gluten</Button>
+      <Button onClick={ handleHealthFilters} value="alcohol-free">Alcohol</Button>
+      <Button onClick={ handleHealthFilters} value="vegan">Vegan</Button>
+      <Button onClick={ handleHealthFilters} value="vegetarian">Vegetarian</Button>
+      </ButtonGroup>
+      </div>
+      </div>}
+     { healthFilter && healthFilter.length  && 
+     <div className={classes.root}>
+     <h3 className={classes.instructions}>{healthFilter} recipes:</h3>
+     <form onSubmit={getSearch} className={classes.searchForm} noValidate>
+        <TextField id="outlined-basic" label="enter an ingredient" variant="outlined" color="primary"  type="text" value={search} onChange={updateSearch} />
+        <Button className={classes.searchButton} variant="contained" color="primary" type="submit">
           Search
-        </button>
+        </Button>
       </form>
-      {/* </header> */}
+      </div>
+     }
       <div className="recipes">
-      {recipes.map(recipe => (
+      {recipes && recipes.map(recipe => (
         <Recipe 
         key={recipe.recipe.label}
         title={recipe.recipe.label} 
         calories={recipe.recipe.calories}
         image={recipe.recipe.image}
-        ingredients={recipe.recipe.ingredients} />
+        ingredients={recipe.recipe.ingredients}
+        healthLabels={recipe.recipe.healthLabels}
+        allergen={healthFilter}
+        source={recipe.recipe.source}
+        url={recipe.recipe.url}
+        />
       ))}
     </div>
     </div>
--- src/Recipe.jsx
+++ src/Recipe.jsx
@@ -1,20 +1,138 @@
 import React from 'react';
-import style from './recipe.module.css';
+import { makeStyles, Theme, createStyles} from '@material-ui/core/styles';
+import clsx from 'clsx';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Collapse from '@material-ui/core/Collapse';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import { blueGrey } from '@material-ui/core/colors';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import { Button, Link } from '@material-ui/core';
 
-const Recipe = ({title, calories, image, ingredients}) => {
-    return [
-        <div className={style.recipe}>
-            <h2 className={style.title}>{title}</h2>
-            <ul>
-                {ingredients.map(ingredient => (
-                    <li className={style.text}>{ingredient.text}</li>
-                ))}
-            </ul>
-            <img className={style.image} src={image} alt=""/>
-            <p className={style.calories}>{(calories/5).toFixed(3)} calories</p>
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+      container : {
+          display: 'flex',
+          flexDirection: 'column',
+        },
+        root: {
+      maxWidth: 345,
+      border: '1px solid grey',
+      marginBottom: '1.5rem',
+      borderRadius: '10px'
+    },
+    header: {
+        display: 'flex',
+        justifyContent: 'space-between',
+        margin: '0.5rem',
+    },
+    title: {
+        fontStyle: 'italic',
+        fontSize: '1.2rem',
+        color: '#2a3eb1',
+        fontWeight: 600,
+        marginTop: '0.2rem',
+    },
+    media: {
+      height: 0,
+      paddingTop: '56.25%', // 16:9,
+    },
+    expand: {
+      transform: 'rotate(0deg)',
+      marginLeft: 'auto',
+      transition: theme.transitions.create('transform', {
+        duration: theme.transitions.duration.shortest,
+      }),
+    },
+    expandOpen: {
+      transform: 'rotate(180deg)',
+    },
+    avatar: {
+      backgroundColor: blueGrey,
+    },
+    link: {
+    textDecoration: 'none',
+    marginLeft: '70%',
+    },
+  }),
+);
+
+const Recipe = ({title, calories, image, ingredients, healthLabels, source, allergen, url}) => {
+
+    const classes = useStyles();
+    const [expanded, setExpanded] = React.useState(false);
+  
+    const handleExpandClick = () => {
+      setExpanded(!expanded);
+    };
+
+    return (
+        <div className={classes.container}>
+        <Card className={classes.root}>
+        <div className={classes.header}>
+              <Avatar aria-label="recipe" className={classes.avatar}>
+                {allergen}
+              </Avatar>
+        <Typography className={classes.title}>{title}</Typography>
         </div>
-    ]
+          <CardMedia
+            className={classes.media}
+            image={image}
+            title={title}
+          />
+          <Typography>Source : {source} </Typography>
+          <CardContent>
+            <Link href={url} target="_blank" rel="noopener" className={classes.link} >
+        <Button className={classes.articleButton} variant="outlined" color="primary" type="submit">
+          Show
+        </Button>
+        </Link>
+            <Typography variant="body2" color="textSecondary" component="p">
+              {(calories/5).toFixed(3)} calories/pers.
+            </Typography>
+
+             {/* <ul>
+                {healthLabels.map(healthLabel => (
+                <li className="">{healthLabel}</li>
+                ))}
+             </ul>   */}
+          </CardContent>
+          <CardActions disableSpacing>
+          <Typography>INGREDIENTS</Typography>
+            <IconButton
+              className={clsx(classes.expand, {
+                [classes.expandOpen]: expanded,
+              })}
+              onClick={handleExpandClick}
+              aria-expanded={expanded}
+              aria-label="show more"
+            >
+              <ExpandMoreIcon />
+            </IconButton>
+          </CardActions>
+          <Collapse in={expanded} timeout="auto" unmountOnExit>
+            <CardContent>
+              <Typography>
+              <ul>
+                {ingredients.map(ingredient => (
+                 <li>{ingredient.text}</li>
+                ))}
+              </ul>
+              </Typography>
+
+            </CardContent>
+          </Collapse>
+        </Card>
+        </div>
+      );
 }
 
 
 export default Recipe;
+
